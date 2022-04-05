8ea63d91d8f4c92fe97e469abbc557b5d09b00d7
Florentin Bürvenich <Florentin@Familie-Buervenich.de>
2022-04-05 22:38:07 +0200

    Add first design draft

--- src/components/CvTemplate.tsx
+++ src/components/CvTemplate.tsx
@@ -4,22 +4,28 @@ import Header from './Header';
 
 function CvTemplate(): JSX.Element {
   return (
-    <div className="result-preview">
+    <div>
       {new Array(5).fill(null).map((v, i) => (
         <div className="page">
-          <Header />
-          {new Array(8).fill(null).map((iv, ii) => (
-            <div>
-              {i}-{ii}. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-              sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-              aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
-              duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-              takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor
-              sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-              tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-              voluptua. At vero eos et accusam et justo duo dolores et ea
+          <div className="stripes-left" />
+          <div className="page-wrapper">
+            <Header />
+            <div className="page-content">
+              {new Array(8).fill(null).map((iv, ii) => (
+                <div>
+                  {i}-{ii}. Lorem ipsum dolor sit amet, consetetur sadipscing
+                  elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+                  dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+                  accusam et justo duo dolores et ea rebum. Stet clita kasd
+                  gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+                  amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                  sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                  magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+                  et justo duo dolores et ea
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
       ))}
     </div>
